a10928d68bb630ba21d10c5bbfaa86d097d4f5f4
Misaki Bear <51944655+MisakiBear@users.noreply.github.com>
2020-12-20 21:51:37 +0900

    Improve reportsearcher

--- WebClassExtension/dist/js/reportsearcher.js
+++ WebClassExtension/dist/js/reportsearcher.js
@@ -1,6 +1,5 @@
 "use strict";
-// The script must be execute after all frames loaded
-$(window).on('load', function () {
+$('frame[name="answer"]').on('load', function () {
     var isexist = $('frame[name="answer"]').contents().find('input[name="report_upload"]').length;
     if (isexist != 0)
         chrome.runtime.sendMessage({ type: 'reportfinded' });
--- WebClassExtension/src/js/reportsearcher.ts
+++ WebClassExtension/src/js/reportsearcher.ts
@@ -1,5 +1,4 @@
-// The script must be execute after all frames loaded
-$(window).on('load', () => {
+$('frame[name="answer"]').on('load', () => {
     let isexist = $('frame[name="answer"]').contents().find('input[name="report_upload"]').length;
     if (isexist != 0)
         chrome.runtime.sendMessage({ type: 'reportfinded' });
